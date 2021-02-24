a4709d736747603592672dab21af233329cc49fe
Syad Khan <syad@student.ubc.ca>
2021-02-23 20:20:10 -0500

    added units tests for catbind

--- tests/test_pypkgs_syadk.py
+++ tests/test_pypkgs_syadk.py
@@ -1,5 +1,13 @@
 from pypkgs_syadk import __version__
 from pypkgs_syadk import pypkgs_syadk
+import pandas as pd
 
 def test_version():
     assert __version__ == '0.1.0'
+
+def test_catbind():
+    a = pd.Categorical(["character", "hits", "your", "eyeballs"])
+    b = pd.Categorical(["but", "integer", "where it", "counts"])
+    assert ((pypkgs_syadk.catbind(a, b)).codes == [1, 4, 7, 3, 0, 5, 6, 2]).all()
+    assert ((pypkgs_syadk.catbind(a, b)).categories == ["but", "character",
+            "counts", "eyeballs", "hits", "integer", "where it", "your"]).all()
